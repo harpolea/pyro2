231d7f8da445ec764819412a3e808984f6ec2e24
harpolea <ah1e14@soton.ac.uk>
2015-11-25 15:15:01 +0000

    Changed flame initial conditios solver to 1d

--- lm_gr/problems/flame.py
+++ lm_gr/problems/flame.py
@@ -71,14 +71,52 @@ def init_data(my_data, aux_data, base, rp, metric):
     scalar.d[:,:] = 0.
     DX.d[:,:] = 0.
 
+    # FIXME: make this into a scalar problem as it's not enjoying this
+
+    d1_u = dens_base
+    p1_u = K * d1_u**gamma
+    e1_u = p1_u / (gamma - 1.0) / d1_u
+    h1_u = 1. + e1_u + p1_u / d1_u
+    # calculate flame speed here
+    u1_u = 1.e2
+    W1 = 1. / np.sqrt(1. - u1_u**2/c**2)
+    J1 = d1_u * W1 * u1_u
+
+    u1_b = u1_u*1.05
+    v1 = 0.
+
+    u1_b = scipy.optimize.fsolve(get_u1_b, u1_b, args=(h1_u*W1, d1_u*W1*u1_u, u1_u, h1_u, p1_u, v1, gamma, c))
+
+    # hW, rhoWv, v_u, h_u, p_u, v, gamma, metric
+    W1_b = 1. / np.sqrt(1. - u1_b**2/c**2)
+    d1_b = J1 / (W1_b * u1_b)
+    h1_b = h1_u * W1 / W1_b
+    p1_b = p1_u - J1*2 * (h1_b/d1_b - h1_u/d1_u)
+
+    # put variables back in
+    idx = (myg.x <= 0.2 * xctr)
+    pres.d[idx] = p1_b
+    pres.d[~idx] = p1_u
+    dens.d[idx] = d1_b
+    dens.d[~idx] = d1_u
+    enth.d[idx] = h1_b
+    enth.d[~idx] = h1_u
+    u.d[idx] = u1_b - u1_u
+    u.d[~idx] = 0.
+
+    dens.v()[:,:] *= np.exp(-g * myg.y[np.newaxis, myg.jlo:myg.jhi+1] /
+                (gamma * c**2 * R * metric.alpha.v2d()**2))
+
+    pres.d[:,:] = K * dens.d**gamma
+    eint.d[:,:] = pres.d / (gamma - 1.0) / dens.d
+    enth.d[:, :] = 1. + eint.d + pres.d / dens.d
 
     # do burnt stuff - shall start flame on left, 10% of the way across the domain
-    idx = (myg.x <= 0.2 * xctr)
     scalar.d[idx] = 1.
     DX.d[idx] = 1.
     # need to increase/decrease other quantities here as well to get the discontinuity - shall use Rankine-Hugoniot stuff
     # u subscript = unburnt, b subscript = burnt
-    p_u = myg.scratch_array()
+    """p_u = myg.scratch_array()
     p_b = myg.scratch_array()
     p_u.d[:,:] = pres.d
     d_u = myg.scratch_array()
@@ -100,30 +138,14 @@ def init_data(my_data, aux_data, base, rp, metric):
     # first estimate
     u_b.d[:,:] = u_u.d
 
-    #res = get_v_b(u_b_low.d, h_u*W_u, d_u*W_u*u_u, u_u, h_u, p_u, v, gamma, metric, myg)
-    #print('Res = ', res)
-
-    u_b.d[:,:] = scipy.optimize.fsolve(get_v_b, u_b.d,  args=(h_u*W_u, d_u*W_u*u_u, u_u, h_u, p_u, v, gamma, metric, myg))
+    v_b = scipy.optimize.fsolve(get_v_b, u_b.d,  args=(h_u*W_u, d_u*W_u*u_u, u_u, h_u, p_u, v, gamma, metric, myg), maxfev=10)
+    u_b.d[:,:] = v_b.reshape((myg.qx, myg.qy))
 
     # hW, rhoWv, v_u, h_u, p_u, v, gamma, metric
     W_b = metric.calcW(u=u_b, v=v)
     d_b.d[:,:] = J.d / (W_b.d * u_b.d)
     h_b.d[:,:] = h_u.d * W_u.d / W_b.d
     p_b.d[:,:] = p_u.d - J.d**2 * (h_b.d/d_b.d - h_u.d/d_u.d)
-
-    # I think the weak deflagration must take the positive root
-    #enth.d[idx] = 0.5 * (p2.d[idx] - p1.d[idx]) / d1.d[idx] + np.sqrt(h2.d[idx]**2 + h2.d[idx] * (p1.d[idx] - p2.d[idx]) / d2.d[idx] + (0.5 * (p1.d[idx] - p2.d[idx]) / d1.d[idx])**2 )
-    # smooth
-    #pres.d[:,:] += (pres.d-p2) * 0.5 * \
-    #    (1. + np.tanh(((myg.x2d-0.2*xctr)/L_x)/0.9))
-    #dens.d[:,:] += (dens.d-d2) * 0.5 * \
-    #    (1. + np.tanh((0.1 * (myg.x2d-0.2*xctr)/L_x)/0.9))
-    #enth.d[:,:] += (enth.d-h2) * 0.5 * \
-    #    (1. + np.tanh(((myg.x2d-0.2*xctr)/L_x)/0.9))
-    #DX.d[:,:] += 0.5 * (1. + np.tanh((-(myg.x2d-0.2*xctr)/L_x)/0.9))
-
-    #Jsq.d[idx] = - (p2.d[idx] - p1.d[idx])/ (h2.d[idx]/d2.d[idx] - h1.d[idx]/d1.d[idx])
-    #u.d[idx] = np.sqrt(Jsq.d[idx]) / d1.d[idx]
 
     # put variables back in
     pres.d[idx] = p_b.d[idx]
@@ -134,7 +156,7 @@ def init_data(my_data, aux_data, base, rp, metric):
     enth.d[~idx] = h_u.d[~idx]
     u.d[idx] = u_b.d[idx] - u_u.d[idx]
     u.d[~idx] = 0.
-
+    """
     my_data.fill_BC_all()
 
     # do the base state
@@ -142,9 +164,10 @@ def init_data(my_data, aux_data, base, rp, metric):
     old_p0 = base["old_p0"]
     D0 = base["D0"]
     Dh0 = base["Dh0"]
-    D0.d[:] = np.mean(dens.d, axis=0)
-    Dh0.d[:] = np.mean(enth.d, axis=0)
-    p0.d[:] = np.mean(pres.d, axis=0)
+    # take means of unburnt
+    D0.d[:] = np.mean(dens.d[~idx], axis=0)
+    Dh0.d[:] = np.mean(enth.d[~idx], axis=0)
+    p0.d[:] = np.mean(pres.d[~idx], axis=0)
 
     u0 = metric.calcu0()
     p0.d[:] = K * (D0.d / u0.d1d())**gamma
@@ -186,6 +209,19 @@ def get_v_b(v_b, hW, rhoWv, v_u, h_u, p_u, v, gamma, metric, myg):
     residual = lhs - rhs
 
     return residual.flatten()
+
+def get_u1_b(v_b, hW, rhoWv, v_u, h_u, p_u, v, gamma, c):
+
+    W_b = 1. / np.sqrt(1. - v_b**2/c**2)
+
+    h_b = hW / W_b
+    p_b = ((h_b - 1.) * rhoWv * (gamma - 1.)) / (gamma * W_b * v_b)
+    lhs = h_b**2 - h_u**2
+    rhs = (hW/rhoWv) * (v_b + v_u) * (p_b - p_u)
+
+    residual = lhs - rhs
+
+    return residual
 
 def calc_flame_speed(myg):
     s = myg.scratch_array()
